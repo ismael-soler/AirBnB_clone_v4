4eec644c2124909fa1132f3df237347933b8be56
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-22 19:37:22 -0300

    commit

--- README.md
+++ README.md
@@ -164,4 +164,9 @@ Second part of Airbnb: Joann Vuong
 ## License
 Public Domain. No copy write protection.
 
+<!--TO START THE SERVER -->
 HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db python3 -m web_dynamic.0-hbnb
+<!--TO START THE API -->
+HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_PORT=5001 python3 -m api.v1.app
+<!--API STATUS REQUEST AT -->
+api: http://localhost:5001/api/v1/status/
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+  // CHECKBOXES LOADING
+  const checkboxesArray = Array.from(document.getElementsByClassName('checkbox'));
+  var amenitiesChecked = [];
+
+  // MAIN CODE HERE
+  checkboxesArray.forEach((checkbox, i) => {
+    checkbox.addEventListener('change', () => {
+      var amenityName = checkbox.getAttribute('data-name');
+
+      if (checkbox.checked) {
+        // adds to the array
+        amenitiesChecked.push(amenityName);
+        modifyHTML(amenitiesChecked);
+      } else {
+        // removes from the array
+        amenitiesChecked.splice(amenitiesChecked.indexOf(amenityName), 1);
+        modifyHTML(amenitiesChecked);
+      }
+    });
+  });
+
+  // API AVAILABILITY
+  const api_status = document.getElementById('api_status');
+
+  fetch('http://localhost:5001/api/v1/status/')
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'OK') {
+        api_status.classList.add('available');
+      } else {
+        api_status.classList.remove('available');
+      }
+    });
+};
+
+function modifyHTML (amenitiesChecked) {
+  // TEXT LOADING
+  const selectedAmenitiesString = Array.from(document.getElementsByClassName('selectedAmenities'));
+  if (amenitiesChecked) {
+    selectedAmenitiesString[0].innerHTML = amenitiesChecked.join(', ');
+  } else {
+    selectedAmenitiesString[0].innerHTML = '&nbsp;';
+  }
+}
